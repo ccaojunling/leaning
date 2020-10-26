22b3eb88447ced3265bd07f38bc4e108d96a02e6
Your Name <you@example.com>
2020-10-26 22:28:24 +0800

    20201025练习

--- web/PageObject/page/base_page.py
+++ web/PageObject/page/base_page.py
@@ -1,17 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class BaseMethod():
-    def __init__(self,driver:WebDriver=None):
+    def __init__(self, driver:WebDriver = None):
         if driver == None:
             # 复用浏览器
             options = Options()
             options.debugger_address="localhost:9222"
             self.driver= webdriver.Chrome(options=options)
+            self.driver.get("https://work.weixin.qq.com/wework_admin/frame#index")
+            self.driver.implicitly_wait(5)
         else:
-            self.driver = webdriver.Chrome()
+            self.driver = driver
 
     def find(self,by,locator):
         element = self.driver.find_element(by,locator)
@@ -20,3 +25,7 @@ class BaseMethod():
     def finds(self,by,locator):
         elements = self.driver.find_elements(by,locator)
         return elements
+
+    def wait_for_click(self,locator,timeout=10):
+        element:WebElement = WebDriverWait(self.driver,timeout).until(expected_conditions.element_to_be_clickable(locator))
+        return element
--- web/PageObject/testcase/test_add_member.py
+++ web/PageObject/testcase/test_add_member.py
@@ -2,7 +2,18 @@ from web.PageObject.page.main_page import MainPage
 
 
 class TestMember:
-    def test_member(self):
-        def setup():
-            self.main = MainPage()
+
+    def setup(self):
+        self.main = MainPage()
+
+    def teardown(self):
+        self.main.driver.quit()
+
+    def test_addmember(self):
+        username='张三27'
+        memberid='1234312317'
+        telephone='13312341247'
+        addmember = self.main.goto_add_member()
+        addmember.add_member_info(username, memberid, telephone)
+        assert addmember.get_member(username)
 
