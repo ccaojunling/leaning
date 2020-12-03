bb6ff060d2880a83dbc73e76819434b062e06594
ccaojunling <1356894882@qq.com>
2020-12-03 17:08:46 +0800

    11

--- mitmproxy/rewrite_xueqiu.py
+++ mitmproxy/rewrite_xueqiu.py
@@ -3,9 +3,18 @@ import json
 from mitmproxy import http
 
 
-def request(flow: http.HTTPFlow) -> None:
+def response(flow: http.HTTPFlow) -> None:
     if "quote.json" in flow.request.pretty_url and "x=" in flow.request.pretty_url:
         data = json.loads(flow.response.content)
-        data['data']['items']
+        data['data']['items'][1]['quote']['name']= ""
+        flow.response.text = json.dumps(data)
 
-        )
+def json_travel(data,array,text,num):
+    """
+
+    :param data: 传入要修改的数据
+    :param array:
+    :param text:
+    :param num:
+    :return:
+    """
